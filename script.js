37604aefe9059a2f72962f85295bda77ea24cd50
thaNorth$tar <114375239+thaNorthStar@users.noreply.github.com>
2022-10-30 09:52:03 -0500

    Lesson 8
    next final lesson 9

--- script.js
+++ script.js
@@ -114,6 +114,52 @@ window.addEventListener('load', function(){
     }
 
     class Object {
+        constructor(game){
+            this.game = game;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    class Bush extends Object{
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('bush');
+            this.imageWidth = 216;
+            this.imageHeight = 100;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
+        }
+    }
+    class Plant extends Object{
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('plant');
+            this.imageWidth = 212;
+            this.imageHeight = 118;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
+        }
+    }
+    class Grass extends Object{
+        constructor(game){
+            super(game);
+            this.game = game;
+            this.image = document.getElementById('grass');
+            this.imageWidth = 103;
+            this.imageHeight = 182;
+            this.width = this.imageWidth;
+            this.height = this.imageHeight;
+            this.x = Math.random() * this.game.width - this.width;
+            this.y = this.game.topMargin + Math.random() * (this.game.height - this.height - this.game.topMargin);
+        }
     }
 
     class Game {
@@ -124,15 +170,29 @@ window.addEventListener('load', function(){
             this.lastKey = undefined;
             this.input = new InputHandler(this);
             this.owlbear = new Owlbear(this);
+            this.numberOfPlants = 10;
+            this.plants = [];
         }
 
         render(context, deltaTime){
             this.owlbear.draw(context);
             this.owlbear.update(deltaTime);
+            this.plants.forEach(plant => plant.draw(context));
+        }
+
+        init(){
+            
+            for (let i = 0; i < this.numberOfPlants; i++){
+                const randomize = Math.random();
+                if (randomize < 0.3) this.plants.push(new Plant(this));
+                else if (randomize < 0.6) this.plants.push(new Bush(this));
+                else this.plants.push(new Grass(this));
+            }
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
+    game.init();
     let lastTime = 0;
     function animate(timeStamp){
         const deltaTime = timeStamp - lastTime;
